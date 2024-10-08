bd423c49a1e8ddbf871a9b634e6ca3e8b6ea165e
ilgertshehaj <shehaji@wit.edu>
2024-10-08 13:15:53 -0400

    Test cases are failing

--- src/edu/wit/cs/comp1000/PA2a.java
+++ src/edu/wit/cs/comp1000/PA2a.java
@@ -1,11 +1,70 @@
 package edu.wit.cs.comp1000;
 
-// TODO: document this class
+import java.util.Scanner;
+
 public class PA2a {
+    public static int sumPos = 0; 
+    public static int sumNeg = 0;
+    public static int countPos = 0;
+    public static int countNeg = 0;
+    public static int totalSum = 0;
 
-	// TODO: document this function
-	public static void main(String[] args) {
-		// TODO: write your code here
-	}
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.printf("Enter five whole numbers: ");
+        
+        for (int i = 0; i < 5; i++) {
+            int userInput = input.nextInt();
+            totalSum += userInput;
+            if (userInput > 0) {
+                sumPos += userInput;
+                countPos++;
+            } else {
+                sumNeg += userInput;
+                countNeg++;
+            }
+        }
 
+        double posAverage = 0.0;
+        if (countPos > 0) {
+            posAverage = (double) sumPos / countPos;
+        }
+
+        double negAverage = 0.0;
+        if (countNeg > 0) {
+            negAverage = (double) sumNeg / countNeg;
+        }
+
+        double averageAll = (double) totalSum / 5;
+
+        // Output results
+        if (countPos == 1) {
+            System.out.printf("The sum of the %d positive number: %d\n", countPos, sumPos);
+        } else {
+            System.out.printf("The sum of the %d positive numbers: %d\n", countPos, sumPos);
+        }
+
+        if (countNeg == 1) {
+            System.out.printf("The sum of the %d non-positive number: %d\n", countNeg, sumNeg);
+        } else {
+            System.out.printf("The sum of the %d non-positive numbers: %d\n", countNeg, sumNeg);
+        }
+
+        System.out.printf("The sum of the 5 numbers: %d\n", totalSum);
+
+        // Averages
+        if (countPos == 1) {
+            System.out.printf("The average of the %d positive number: %.2f\n", countPos, posAverage);
+        } else {
+            System.out.printf("The average of the %d positive numbers: %.2f\n", countPos, posAverage);
+        }
+
+        if (countNeg == 1) {
+            System.out.printf("The average of the %d non-positive number: %.2f\n", countNeg, negAverage);
+        } else {
+            System.out.printf("The average of the %d non-positive numbers: %.2f\n", countNeg, negAverage);
+        }
+
+        System.out.printf("The average of the 5 numbers: %.2f\n", averageAll);
+    }
 }
